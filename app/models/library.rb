1605b596969a81e3fbe3e9097018b1a99f782d5d
Tanuka16 <tanuka.das12@gmail.com>
2020-01-26 14:36:45 -0500

    added models and more tables in schema

---
+++ app/models/library.rb
@@ -0,0 +1,6 @@
+class Library < ApplicationRecord
+  belongs_to :book
+  belongs_to :user
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_211155) do
+ActiveRecord::Schema.define(version: 2020_01_26_170817) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2020_01_23_211155) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "libraries", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,6 +58,14 @@ ActiveRecord::Schema.define(version: 2020_01_23_211155) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_id"
+    t.string "stripe_subscription_id"
+    t.string "card_last4"
+    t.integer "card_exp_month"
+    t.integer "card_exp_year"
+    t.string "card_type"
+    t.boolean "admin"
+    t.boolean "subscribed"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
