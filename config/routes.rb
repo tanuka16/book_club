204f35a0ac6f4911a13d107aeb666ea97bee9be1
Tanuka16 <tanuka.das12@gmail.com>
2020-02-02 14:09:38 -0500

    archetectural setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :books
-  resources :library, only [:index]
-  devise_for :users
+  resources :books do                                         #restful routes and inside it add more that aren't restful
+    member do
+      put "add", to: "books#library"
+      put "remove", to: "books#library"
+    end
+  end
+  resources :library, only:[:index]
+  resources :pricing, only:[:index]
+  devise_for :users, controller: { registrations: "registrations"}
   root to: 'books#index'
+  resources :subscriptions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
